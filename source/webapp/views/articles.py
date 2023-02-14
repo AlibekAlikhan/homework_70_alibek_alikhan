4c0ae3215da5d433e39ccc52a92cbbe3e28bad61
Alibek Alikhan <alialikhan0520@gmail.com>
2023-02-14 16:10:34 +0600

    Отредактировал url и articles файлы и добавил  html файлы

--- source/webapp/views/articles.py
+++ source/webapp/views/articles.py
@@ -1,37 +1,12 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 
-from webapp.views.db import Cat
-
-c = Cat()
+from webapp.models import Article
 
 
-def add_view(request: WSGIRequest):
-    if request.method == "GET":
-        return render(request, "article_create.html")
-    print(request.POST)
-
-
-def state_cat(make):
-    match make:
-        case "play":
-            c.play()
-        case "feed":
-            c.eat()
-        case "sleep":
-            c.slepp()
-
-
-def cat_stats(request: WSGIRequest):
-    make = request.POST.get('make')
-    name = request.GET.get('name')
-    state_cat(make)
-    return render(request, 'article.html', context={
-        'name': name,
-        'year': c.year,
-        'happiness': c.happiness,
-        'satiety': c.satiety,
-        'make': make,
-        'sleep': c.sleep,
-        'img': c.img
-    })
+def article_view(request: WSGIRequest):
+    articles = Article.objects.all()
+    context = {
+        "articles": articles
+    }
+    return render(request, "tasks.html", context=context)
