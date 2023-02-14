112030bd888523178eba8f8342842c8c126aabe6
Alibek Alikhan <alialikhan0520@gmail.com>
2023-02-14 15:18:40 +0600

    Создал и заполнил базу

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from webapp.models import Article
+
+
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("id", "status", "create_at")
+    list_filter = ("id", "status", "create_at")
+    search_fields = ("status", "text")
+    filter = ("text", "status", "create_at")
+    readonly_fields = ("id", "create_at", "end_at")
+
+
+admin.site.register(Article, ArticleAdmin)
