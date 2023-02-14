130405ba733183f4c72405c59a15bd91c85aa980
Alibek Alikhan <alialikhan0520@gmail.com>
2023-02-14 17:21:11 +0600

    Отредактировал html

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views.articles import article_view
+from webapp.views.articles import article_view, article_create
 
 urlpatterns =[
-    path('', article_view)
+    path('', article_view),
+    path('article/create', article_create)
 ]
--- source/webapp/views/articles.py
+++ source/webapp/views/articles.py
@@ -1,5 +1,5 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from webapp.models import Article
 
@@ -10,3 +10,16 @@ def article_view(request: WSGIRequest):
         "articles": articles
     }
     return render(request, "tasks.html", context=context)
+
+
+def article_create(request: WSGIRequest):
+    if request.method == "GET":
+        return render(request, "article_create.html")
+    print(request.POST)
+    article_data = {
+        "status": request.POST.get('status'),
+        "text": request.POST.get('text'),
+        "create_at": request.POST.get('create_at')
+    }
+    Article.objects.create(**article_data)
+    return redirect(f"/")
