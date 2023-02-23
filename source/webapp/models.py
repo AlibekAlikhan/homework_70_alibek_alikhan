a72c0a9f1bc7f746d5694510f81dac17fe161d91
Alibek Alikhan <alialikhan0520@gmail.com>
2023-02-23 17:35:17 +0600

    Исправил миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Article(models.Model):
     status = models.CharField(max_length=10, null=False, verbose_name="Статус")
     text = models.TextField(max_length=3000, null=True, verbose_name="Текст")
+    detail_text = models.TextField(max_length=3000, null=True, verbose_name="Детальный текст")
     create_at = models.DateField(verbose_name="Дата добавления")
 
 
